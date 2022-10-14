ed36aa6720d6a766708356207a58e0f9a9686f9e
Laetitia Taddei <laetdi@gmail.com>
2022-10-14 17:23:53 +0200

    create a projectData component with carearound and master jedi content

---
+++ src/pages/Projects/ProjectData.jsx
@@ -0,0 +1,16 @@
+import CareAroundMedia from "../../utils/videos/care_around.mp4"
+import MasterJediMedia from "../../utils/videos/jedi_master.mp4"
+
+export const ProjectData = [
+  {
+    media: {CareAroundMedia},
+    title: "CareAround",
+    descriptionText: "A mobile application using Optical Character Recognition (Google cloud OCR API) on beauty products and finding a match in a Japanese products database,built using scrapping with Kimurai gem allowing infinite scroll. Scrapped database translated from japanese to english through DEEPL API. Product présentation"
+  },
+
+  {
+    media: {MasterJediMedia},
+    title: "Master Jedi",
+    descriptionText: "A web application build on ruby on rails with an exagerated design and javascript effect."
+  }
+]
