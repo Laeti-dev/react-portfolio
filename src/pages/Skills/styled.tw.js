893de6660a58adcbe2b45ac855f8a18db35383fa
Laeti-dev <laetdi@gmail.com>
2024-01-25 05:24:21 +0100

    Adjust col screen

--- src/pages/Skills/styled.tw.js
+++ src/pages/Skills/styled.tw.js
@@ -1,52 +1,48 @@
 import tw from "tailwind-styled-components";
 
-export const Skills = tw.ul`
-  mx-auto
+export const Skills = tw.section`
   grid
+  mx-auto
 
-  sm:mt-12
-  sm:grid-cols-3
-  sm:gap-8
+  sm:grid-cols-4
+  sm:gap-5
+  sm:mt-16
 
-  md:mt-15
-  md:grid-cols-5
-  md:gap-7
+  md:grid-cols-4
+  md:mt-8
+  md:gap-8
 
   lg:mt-24
+  lg:grid-cols-5
 
-`
-
-export const SkillsLogo = tw.img`
-sm:max-w-12
-sm:h-12
-
-md:w-16
-md:h-16
-
-lg:w-20
-lg:h-20
 `;
 
+export const SkillsLogo = tw.img`
+sm:w-12
+sm:h-12
+
+md:w-28
+md:h-28
+`;
 export const TabMenu = tw.menu`
 flex
-gap-2
-list-none
-mx-4
-my-0
-p-0
+gap-6
+mt-4
 
 md:mx-4
 md:my-0
 md:p-0
+
+
+list-none
 `
 
 export const TabButtonSt = tw.button`
 border-none
 rounded-md
-text-nav-bar-color
 bg-transparent
-hover:text-active-color
-
+text-nav-bar-color
+active:text-active-color
 cursor-pointer
 transition-all
 ease-out
@@ -54,8 +50,7 @@ duration-75
 
 md:px-2
 md:py-0
-
-
-
+md:flex
+md:gap-6
 
 `
